379c791389f397578fb0b4cd18ce1e306f8f2216
Fabian Piñatares <fpinatares@gmail.com>
2019-11-09 14:12:50 -0300

    Values checked

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package couponEvaluator
 
 import (
-	"fmt"
-
 	"github.com/Knetic/govaluate"
 )
 
 func Evaluate(required_keys []string, values map[string]interface{}, condition string) bool {
-	expression, err := govaluate.NewEvaluableExpression(condition)
-	result, err := expression.Evaluate(values)
-	fmt.Println(result)
-	fmt.Println("ERROR ", err)
-	return false
+	if CheckValidValues(required_keys, values) {
+		expression, err := govaluate.NewEvaluableExpression(condition)
+		result, err := expression.Evaluate(values)
+		return err != nil && result.(bool)
+	} else {
+		return false
+	}
+}
+
+func CheckValidValues(required_keys []string, values map[string]interface{}) bool {
+	valid := true
+	for i := range required_keys {
+		if _, ok := values[required_keys[i]]; !ok {
+			valid = false
+		}
+	}
+	return valid
 }
