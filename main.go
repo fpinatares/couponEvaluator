12f2ad680baa2ed970dea4396d4bae8c104f8d18
Fabian Piñatares <fpinatares@gmail.com>
2019-11-16 00:45:55 -0300

    Fix bug

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func Evaluate(requiredKeys []string, values map[string]interface{}, condition st
 	})*/
 	bodyToRequest, err := json.Marshal(bodyToEvaluate)
 	response, err := http.Post("localhost:8082/evaluate", "application/json", bytes.NewBuffer(bodyToRequest))
-	defer response.Body.Close()
+	//defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatalln(err)
